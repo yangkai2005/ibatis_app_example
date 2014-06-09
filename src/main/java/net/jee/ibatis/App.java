43424403740e89173f7316d4330a5bceb17148b9
yangkai2005 <yangkai_2005@163.com>
2014-06-09 22:59:03 +0800

    create project

---
+++ src/main/java/net/jee/ibatis/App.java
@@ -0,0 +1,13 @@
+package net.jee.ibatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
